e6591e2c8e807628642a0f908237fe5a132ebb36
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-09-12 18:46:55 +0000

    Create palindrome.cpp

---
+++ Chapter 4/palindrome.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+//function to remove spaces
+string removeSpaces (const string& input) {
+   string result;
+   
+   for (char character : input) {
+      if (character != ' ') {
+         result += character;
+         }
+      }
+      
+      return result;
+   }
+   
+bool isPalindrome(const string& phrase) {
+   int start = 0;
+   int end = phrase.length() - 1;
+   
+   while (start < end) {
+      while (!isalnum(phrase[start])) {
+         start++;
+      }
+      while (!isalnum(phrase[end])) {
+         end--;
+      }
+       
+      if (tolower(phrase[start]) != tolower(phrase[end])) {
+         return false;
+      }
+         
+      start++;
+      end--;
+      
+   }
+   
+      return true;
+}
+
+int main() {
+
+   string withspaces;
+   
+   cin >> withspaces;
+   
+   string nospaces = removeSpaces(withspaces);
+   
+   if(isPalindrome(nospaces)) {
+      cout << "Palindrome: " << withspaces << endl;
+   } else {
+      cout << "not a palindrome: " << withspaces << endl;
+   }
+   
+   
+   
+
+   return 0;
+}
